1e1ff1a6fc0620c627f8370281db4ea8d881bf5b
Fabio Rosato <far@bendingspoons.com>
2022-07-25 22:23:08 +0200

    chore: update siteConfig.js vars

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,13 +1,13 @@
-export const SITE_URL = 'https://swyxkit.netlify.app';
-export const APPROVED_POSTERS_GH_USERNAME = ['sw-yx'];
-export const GH_USER_REPO = 'sw-yx/swyxkit'; // used for pulling github issues and offering comments
+export const SITE_URL = 'https://fabiorosatodotcom.netlify.app/';
+export const APPROVED_POSTERS_GH_USERNAME = ['farosato'];
+export const GH_USER_REPO = 'farosato/fabiorosatodotcom'; // used for pulling github issues and offering comments
 export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
-export const SITE_TITLE = 'SwyxKit';
-export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
+export const SITE_TITLE = 'Fabio Rosato';
+export const SITE_DESCRIPTION = "Fabio Rosato's site";
 export const DEFAULT_OG_IMAGE =
-	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
-export const MY_TWITTER_HANDLE = 'swyx';
+	'https://pbs.twimg.com/profile_images/1443710468426768385/VFjYjDGu_400x400.jpg';
+export const MY_TWITTER_HANDLE = 'fab_rosato';
 
-// dont forget process.env.GH_TOKEN
+// don't forget process.env.GH_TOKEN
 // if supplied, raises rate limit from 60 to 5000
 // https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting
